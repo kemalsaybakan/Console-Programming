8631db77eca4b2b5f4c85cac239972599bd8a7ce
Kemal Saybakan <kemalsaybakan@gmail.com>
2022-11-01 01:20:48 +0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace console_programming // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // Ask name 
+            Console.WriteLine("Enter your name : ");
+            string name = Console.ReadLine();
+            // Ask surname 
+            Console.WriteLine("Enter your surname : ");
+            string surname = Console.ReadLine();
+
+            // Print name and surname on console 
+            Console.WriteLine("Merhaba " + name + " " + surname);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# .Net 6.0 Console Programming:  
+
+## Console App (.NET Core) | Sample Project
+In our tutorial, we will use Visual Studio Community, which is free to download from https://visualstudio.microsoft.com/vs/community/.
+
+Applications written in C# use the .NET Framework, so it makes sense to use Visual Studio, as the program, the framework, and the language, are all created by Microsoft.
+
+A console application is a program which runs in an command prompt window.
+Following you will find a relatively simple console program that get and print user's name and surname.
+
+[Developer Website](https://www.kemalsaybakan.com/)
+
